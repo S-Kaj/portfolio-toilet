3c40f2af5d9294a192a2f0e868687949ab5c85f6
Shun.K <sh.kajihara.t@gmai.com>
2022-04-05 16:23:06 +0900

    CSSを最低限適用

--- app/controllers/toilets_controller.rb
+++ app/controllers/toilets_controller.rb
@@ -62,6 +62,10 @@ class ToiletsController < ApplicationController
     end
   end
 
+  def myposts
+    @toilets = Toilet.all
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_toilet
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root to: 'toilets#index'
-  resources :toilets
+  resources :toilets do
+    collection do
+      get 'myposts'
+    end
+  end
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
