fe665830c2798ede02c90e9bc38b9eb75b9c9487
Shun.K <sh.kajihara.t@gmai.com>
2022-08-29 11:41:45 +0900

    バリデーションの追加

--- app/models/toilet.rb
+++ app/models/toilet.rb
@@ -1,6 +1,7 @@
 class Toilet < ApplicationRecord
   belongs_to :user
-  validates :toilet_name, presence: true, length: {maximum: 50}, uniqueness:true
+  validates :toilet_name, presence: true, length: {maximum: 30}, uniqueness:true
   validates :latitude, presence: true
   validates :longtitude, presence: true
+  validates :remark, length: {maximum: 100}
 end
