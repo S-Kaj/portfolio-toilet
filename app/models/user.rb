aa252d6ff218aa7ce312051412ce4c873f1afe60
Shun.K <sh.kajihara.t@gmai.com>
2022-08-29 17:49:32 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   has_many :toilets
   has_one_attached :image
+
+  validates :user_name, presence: true, length: {maximum: 20}, uniqueness:true
 end
