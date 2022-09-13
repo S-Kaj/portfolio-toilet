afe378600f32d0fb0641f9cd49bed7d361dc86e2
Shun.K <sh.kajihara.t@gmai.com>
2022-09-13 20:18:23 +0900

    モデルスペックの作成

--- spec/models/toilet_spec.rb
+++ spec/models/toilet_spec.rb
@@ -1,5 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe Toilet, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'トイレ情報の登録時のテスト' do
+    let(:user) { create(:user) }
+
+    it "必要な項目が揃っている場合は有効であること" do
+      toilet = create(:toilet, user_id: user.id)
+      toilet.valid?
+      expect(toilet).to be_valid
+    end
+
+    context 'バリデーションのチェック' do
+      it "トイレ名称が空欄の場合は無効であること" do
+        toilet = build(:toilet, toilet_name: nil, user_id: user.id)
+        toilet.valid?
+        expect(toilet.errors.messages[:toilet_name]).to include("を入力してください")
+      end
+
+      it "トイレ名称が31文字以上の場合は無効であること" do
+        toilet = build(:toilet, toilet_name: "a" * 31 , user_id: user.id)
+        toilet.valid?
+        expect(toilet.errors.messages[:toilet_name]).to include("は30文字以内で入力してください")
+      end
+
+      it "マップ上で位置を未選択の場合は無効であること" do
+        toilet = build(:toilet, latitude: nil, longtitude: nil, user_id: user.id)
+        toilet.valid?
+        expect(toilet.errors.messages[:latitude]).to include("を入力してください")
+      end
+    end
+
+    it "重複したトイレ名称では無効であること" do
+      toilet1 = create(:toilet, toilet_name: "トイレA", user_id: user.id)
+      toilet2 = build(:toilet, toilet_name: "トイレA", user_id: user.id)
+      toilet2.valid?
+      expect(toilet2.errors.messages[:toilet_name]).to include("はすでに存在します")
+    end
+
+    it "備考が101文字以上の場合は無効であること" do
+      toilet = build(:toilet, remark: "a" * 101, user_id: user.id)
+      toilet.valid?
+      expect(toilet.errors.messages[:remark]).to include("は100文字以内で入力してください")
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,63 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "ユーザー登録時のテスト" do
+    it "必要な項目が揃っている場合は有効であること" do
+      user = create(:user)
+      user.valid?
+      expect(user).to be_valid
+    end
+
+    context "バリデーションのチェック" do
+      it "ユーザー名が空欄の場合は無効であること" do
+        user = build(:user, user_name: nil)
+        user.valid?
+        expect(user.errors.messages[:user_name]).to include("を入力してください")
+      end
+
+      it "ユーザー名が21文字以上の場合は無効であること" do
+        user = build(:user, user_name: "a" * 21)
+        user.valid?
+        expect(user.errors.messages[:user_name]).to include("は20文字以内で入力してください")
+      end
+
+      it "ユーザー名が重複する場合は無効であること" do
+        user1 = create(:user, user_name: "山田太郎")
+        user2 = build(:user, user_name: "山田太郎")
+        user2.valid?
+        expect(user2.errors.messages[:user_name]).to include("はすでに存在します")
+      end
+
+      it "メールアドレスが空欄の場合は無効であること" do
+        user = build(:user, email: nil)
+        user.valid?
+        expect(user.errors.messages[:email]).to include("を入力してください")
+      end
+
+      it "メールアドレスが重複する場合は無効であること" do
+        user1 = create(:user, email: "test@example.com")
+        user2 = build(:user, email: "test@example.com")
+        user2.valid?
+        expect(user2.errors.messages[:email]).to include("はすでに存在します")
+      end
+
+      it "パスワードが空欄の場合は無効であること" do
+        user = build(:user, password: nil)
+        user.valid?
+        expect(user.errors.messages[:password]).to include("を入力してください")
+      end
+
+      it "パスワードが6文字未満の場合は無効であること" do
+        user = build(:user, password: "12345")
+        user.valid?
+        expect(user.errors.messages[:password]).to include("は6文字以上で入力してください")
+      end
+
+      it "パスワードとパスワードの確認が一致しない場合は無効であること" do
+        user = build(:user, password: "123456", password_confirmation: "654321")
+        user.valid?
+        expect(user.errors.messages[:password_confirmation]).to include("とパスワードの入力が一致しません")
+      end
+    end
+  end
 end
