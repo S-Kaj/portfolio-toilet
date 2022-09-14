32521da43f9a09aacf19881b8e039e59604a3abf
Shun.K <sh.kajihara.t@gmai.com>
2022-09-14 17:21:20 +0900

    リクエストスペックを作成

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,11 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  describe "GET /show" do
-    it "returns http success" do
-      get "/users/show"
-      expect(response).to have_http_status(:success)
-    end
+  let(:user) { create(:user) }
+  let!(:toilet) { create(:toilet, user_id: user.id) }
+
+  before do
+    sign_in user
   end
 
+  context 'GET mypage_users_path' do
+    it 'ステータスコードが200であること' do
+      get mypage_users_path
+      binding.pry
+      expect(response).to have_http_status(200)
+    end
+
+    it 'レスポンスボディにユーザー情報およびトイレ情報が含まれること' do
+      get mypage_users_path
+      expect(response.body).to include user.user_name
+      expect(response.body).to include user.email
+      expect(response.body).to include user.created_at.strftime("%Y年%-m月%-d日")
+      expect(response.body).to include user.updated_at.strftime("%Y年%-m月%-d日")
+      expect(response.body).to include toilet.toilet_name
+      expect(response.body).to include toilet.created_at.strftime("%Y年%-m月%-d日")
+    end
+  end
 end
