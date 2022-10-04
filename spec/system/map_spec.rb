80d8978201768178bc751ffeb0b9abf0dd932cc7
Shun.K <sh.kajihara.t@gmai.com>
2022-10-04 22:50:00 +0900

    枠内に表示される緯度・経度情報に指定

--- spec/system/map_spec.rb
+++ spec/system/map_spec.rb
@@ -2,10 +2,11 @@ require 'rails_helper'
 
 RSpec.describe "Maps", type: :system do
   let!(:user) { create(:user, email: 'test@example.com', password: '12345678', password_confirmation: '12345678') }
-  let!(:toilet) { create(:toilet) }
+  let!(:toilet) { create(:toilet, latitude: '35.68', longtitude: '139.75') }
 
   scenario 'GoogleMAPが表示されていること', js: true do
     visit root_path
+    binding.pry
     expect(page).to have_selector 'map'
   end
 end
