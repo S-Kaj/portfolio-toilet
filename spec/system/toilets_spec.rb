be758e2e1b780c1f7b28309b16dd63f46d66c04b
Shun.K <sh.kajihara.t@gmai.com>
2022-11-10 17:32:56 +0900

    不要箇所の削除

--- spec/system/toilets_spec.rb
+++ spec/system/toilets_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Toilets", type: :system, selenium: true do
   let!(:user) { create(:user, email: 'test@example.com', password: '12345678', password_confirmation: '12345678') }
 
-  scenario "ユーザーが新しいトイレ情報を登録すること", js: true do
+  scenario "ユーザーが新しいトイレ情報を登録できること", js: true do
     visit root_path
     # ログインボタンを押す
     click_link 'ログイン'
@@ -12,31 +12,17 @@ RSpec.describe "Toilets", type: :system, selenium: true do
     fill_in 'user[password]', with: '12345678'
     # ログインボタンを押下
     click_button 'ログイン'
-    # binding.pry
     expect(current_path).to eq root_path
     # 「トイレ情報の登録」ボタンを押下
     click_link 'トイレ情報の登録'
-
-    page.driver.execute_script('return window.location.pathname')
-
-
-    # 各種項目を入力
+    # 各種項目の入力
     fill_in 'toilet[toilet_name]', with: 'テストトイレ'
-    # マップ上をクリックさせるにはどうしたら良い？(hiddenに値をセットすることで代用)
-    # find('#map_lat', visible: false).set(100)
-    # find('#map_lng', visible: false).set(100)
-    # find('div#map', visible: false).click
-
-    page.save_screenshot
-
-    # 指定した座標をクリックする動作
+    # Googleマップ上にピンを置く
     page.driver.browser.action
       .move_to_location(880, 480)
       .click
       .perform
-    # ここまで
-
-# binding.pry
+    # その他各種項目の入力
     fill_in 'toilet[m_urinal]', with: '4'
     fill_in 'toilet[m_room]', with: '3'
     check 'toilet[m_washlet]'
@@ -50,8 +36,6 @@ RSpec.describe "Toilets", type: :system, selenium: true do
     fill_in 'toilet[remark]', with: '備考テスト'
     # 「登録する」ボタンを押下
     click_button '登録する'
-# binding.pry
-    # expect(page).to have_content '新しいトイレ情報が登録されました。'
-    # puts page.driver.browser.manage.logs.get(:browser)
+    expect(page).to have_content '新しいトイレ情報が登録されました。'
   end
 end
