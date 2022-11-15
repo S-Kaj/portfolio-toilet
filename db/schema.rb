5234e989a7cdd3e04740712526ba43227f601dc4
Shun.K <sh.kajihara.t@gmai.com>
2022-11-15 19:18:37 +0900

    マイグレーションによる追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_13_090942) do
+ActiveRecord::Schema.define(version: 2022_11_15_094350) do
 
   create_table "active_storage_attachments", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2022_04_13_090942) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["toilet_name"], name: "index_toilets_on_toilet_name", unique: true
     t.index ["user_id"], name: "index_toilets_on_user_id"
   end
 
@@ -68,8 +69,10 @@ ActiveRecord::Schema.define(version: 2022_04_13_090942) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "user_name", null: false
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["user_name"], name: "index_users_on_user_name", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
